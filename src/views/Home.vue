02554a1165f063491aa1dd72db607fc70c0555c0
Yeabsira ashenafi <ashenafiyeabsira@gmail.com>
2020-02-03 22:29:49 +0300

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container class="home">
+   <!-- <h1 style="margin-left=90px">Conntainer</h1> -->
+   <login/>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import login from '../components/login';
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+ data(){
+
+ },
+ components: {
+     login
+ }
+
 };
 </script>
+
