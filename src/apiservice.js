1fbb3cb42777ce527f329bd48c79efdae96c9353
Yeabsira ashenafi <ashenafiyeabsira@gmail.com>
2020-02-04 20:08:06 +0300

    adding of Curriculum and connections with the api

--- src/apiservice.js
+++ src/apiservice.js
@@ -2,20 +2,26 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:3000/api';
 export class apiservice{
-    
+
 constructor(){
 
 }
 async register(data){
-    
+
    await axios.post(`${API_URL}/Accounts`,data).then( res => {
        console.log(res);
    });
 
  };
 
- 
+ async addStructure(data){
+   await axios.post(`${API_URL}/y`,data).then( res => {
+     console.log(res);
+   })
+ };
+
+
 };
 
- 
-    
+
+
--- src/components/AddCurrContent.vue
+++ src/components/AddCurrContent.vue
@@ -30,57 +30,61 @@
             ></v-textarea>
             <p outlined class="headline">Program Outcome/s(PO)</p>
             <v-flex class="ml-5">
-              <template v-for="PO in no_of_PO">
-                <v-flex v-bind:key="PO">
+              <template v-for="(program,index) in po">
+                <v-flex v-bind:key="program.index">
                   <v-layout>
-                    <p class="title">PO{{ PO }}</p>
+                    <p class="title"  >PO{{ index+1 }}</p>
                     <v-spacer></v-spacer>
                     <v-icon @click="addPO()">mdi-plus</v-icon>
                   </v-layout>
-                  <v-text-field outlined label="Name of Outcome"></v-text-field>
-                  <v-text-field outlined label="Description"></v-text-field>
+                  <v-text-field outlined label="Name of Outcome" v-model="program.name"></v-text-field>
+                  <v-text-field outlined label="Description" v-model="program.details"></v-text-field>
                 </v-flex>
               </template>
               <p class="headline">Program Educational Outcome/s(PEO)</p>
               <v-flex>
-                <template v-for="PEO in no_of_PEO">
-                  <v-flex v-bind:key="PEO">
+                <template v-for="(PEO,index) in peo">
+                  <v-flex v-bind:key="PEO.index">
                     <v-layout>
-                      <p class="title">PEO{{ PEO }}</p>
+                      <p class="title">PEO{{ index+1 }}</p>
                       <v-spacer></v-spacer>
                       <v-icon @click="addPEO()">mdi-plus</v-icon>
                     </v-layout>
                     <v-text-field
                       outlined
                       label="Name of Educational Outcome"
+                      v-model="PEO.name"
                     ></v-text-field>
-                    <v-text-field outlined label="Description"></v-text-field>
+                    <v-text-field outlined label="Description" v-model="PEO.details"></v-text-field>
                     <v-select
                       multiple
                       :items="POS"
                       label="Mapped PO/POs"
+                      v-model="PEO.mappedPO"
                     ></v-select>
                   </v-flex>
                 </template>
               </v-flex>
               <p class="headline">Curriculum Learning Outcome</p>
               <v-flex>
-                <template v-for="CLO in no_of_CLO">
-                  <v-flex v-bind:key="CLO">
+                <template v-for="(CLO,index) in clo">
+                  <v-flex v-bind:key="CLO.index">
                     <v-layout>
-                      <p class="title">CLO{{ CLO }}</p>
+                      <p class="title" >CLO{{ index+1 }}</p>
                       <v-spacer></v-spacer>
                       <v-icon @click="addCLO()">mdi-plus</v-icon>
                     </v-layout>
                     <v-text-field
                       outlined
                       label="Name of Educational Outcome"
+                      v-model="CLO.name"
                     ></v-text-field>
-                    <v-text-field outlined label="Description"></v-text-field>
+                    <v-text-field outlined label="Description" v-model="CLO.details"></v-text-field>
                     <v-select
                       multiple
                       :items="PEOS"
                       label="Mapped PEO/PEOs"
+                      v-model="CLO.mappedPEO"
                     ></v-select>
                   </v-flex>
                 </template>
@@ -99,6 +103,8 @@
 </template>
 
 <script>
+import {apiservice} from "../apiservice";
+const api = new apiservice();
 export default {
   data: () => {
     return {
@@ -119,38 +125,79 @@ export default {
       rational: "",
       po: [
         {
+          id: "",
           name: "",
-          details: ""
+          details: "",
         }
+      ],
+      peo: [
+        {
+          id:"",
+          name:"",
+          details:"",
+          mappedPO:[]
+        }
+      ],
+      clo: [
+        {
+        id:"",
+        name:"",
+        details:"",
+        mappedPEO:[]
+      }
+
       ]
     };
   },
   methods: {
     addPO() {
       window.alert("added a program outcome");
-      this.no_of_PO++;
+      //this.no_of_PO++;
+      this.po.push({});
     },
     addPEO() {
-      this.no_of_PEO++;
+      this.peo.push({});
     },
     addCLO() {
-      this.no_of_CLO++;
+      this.clo.push({});
     },
     addCurriculum() {
-      this.console.log(this.po);
+      this.setpoID();
+      let data={
+        program_name:this.name,
+        program_type:this.type,
+        background:this.background,
+        rational:this.rational,
+        program_outcome:this.po,
+        program_educational_outcome:this.peo,
+        course_learning_outcome:this.clo,
+        owner:"Admin",
+
+      }
+      console.log(data)
+
+      api.addStructure(data).then((data) => {
+        console.log(data);
+      })
+    },
+    setpoID() {
+      for(var program in this.po){
+        console.log(program)
+        // this.po[i].id = "po"+(i+1)
+      }
     }
   },
   computed: {
     POS: function() {
       var x = [];
-      for (var i = 1; i <= this.no_of_PO; i++) {
+      for (var i = 1; i <= this.po.length; i++) {
         x.push("PO" + i);
       }
       return x;
     },
     PEOS: function() {
       var y = [];
-      for (var j = 1; j <= this.no_of_PEO; j++) {
+      for (var j = 1; j <= this.peo.length; j++) {
         y.push("PEO" + j);
       }
       return y;
