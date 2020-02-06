dd3b1215158477745aee7ee1d798d6f7d744e0ee
Yeabsira ashenafi <ashenafiyeabsira@gmail.com>
2020-02-06 20:45:37 +0300

    adding orgnaizationals

--- src/components/addorganization.vue
+++ src/components/addorganization.vue
@@ -7,19 +7,19 @@
                   <v-flex>
 
                            <v-layout> 
-                            <v-text-field label="Name of the organization" height="30"></v-text-field>
+                            <v-text-field label="Name of the organization" v-model="name" height="30"></v-text-field>
                             <v-spacer></v-spacer>
-                            <v-text-field label="Location"></v-text-field> 
+                            <v-text-field label="Location" v-model="location"></v-text-field> 
                            </v-layout>
-                           <v-text-field label="Background"></v-text-field>
+                           <v-text-field label="Background" v-model="year_of_establishment"></v-text-field>
                           <v-layout width="50%">
                               
-                              <v-text-field label="Year of establishment"></v-text-field>
+                              <v-text-field label="Year of establishment" v-model="year_of_establishment"></v-text-field>
                               <v-file-input label="Logo of the organization"></v-file-input>
                           </v-layout>
-                          <v-text-field label="Motto"></v-text-field>
+                          <v-text-field label="Motto" v-model="Motto"></v-text-field>
                           
-                          <v-btn color="green float-right">Add Organization</v-btn>
+                          <v-btn color="green float-right" @click="add()">Add Organization</v-btn>
                       
                   </v-flex>
               </v-card-actions>
@@ -38,6 +38,18 @@ export default {
          year_of_establishment:'',
          logo:'',
          Motto:''
+     }
+ },
+ methods:{
+     add(){
+         let data={
+             name:this.name,
+             location:this.location,
+             background:this.background,
+             year_of_establishment:this.year_of_establishment,
+             Motto:this.Motto
+         }
+         console.log(data)
      }
  }
 }
