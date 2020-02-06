90c839b6652705a23a496f47cd951143266688e2
Yeabsira ashenafi <ashenafiyeabsira@gmail.com>
2020-02-06 20:39:29 +0300

    adding organizations

--- src/components/addorganization.vue
+++ src/components/addorganization.vue
@@ -1,19 +1,19 @@
 <template>
   <span>
       <v-container>
-          <v-card width="80%" raised>
-              <v-card-title ><p class="heading">Add an organization</p></v-card-title>
+          <v-card width="80%" raised ml-5>
+              <v-card-title ><p class="headline">Add an organization</p></v-card-title>
               <v-card-actions >
                   <v-flex>
 
                            <v-layout> 
                             <v-text-field label="Name of the organization" height="30"></v-text-field>
                             <v-spacer></v-spacer>
-                            <v-text-field label="location"></v-text-field> 
+                            <v-text-field label="Location"></v-text-field> 
                            </v-layout>
                            <v-text-field label="Background"></v-text-field>
                           <v-layout width="50%">
-                              <p>yes</p>
+                              
                               <v-text-field label="Year of establishment"></v-text-field>
                               <v-file-input label="Logo of the organization"></v-file-input>
                           </v-layout>
@@ -24,13 +24,22 @@
                   </v-flex>
               </v-card-actions>
           </v-card>
-      </v-container>d>
+      </v-container>
   </span>
 </template>
 
 <script>
 export default {
-
+ data :() =>{
+     return{
+         name:'',
+         location:'',
+         background:'',
+         year_of_establishment:'',
+         logo:'',
+         Motto:''
+     }
+ }
 }
 </script>
 
--- src/views/addOrgn.vue
+++ src/views/addOrgn.vue
@@ -1,6 +1,6 @@
 <template>
   <span>
-      <addorganization /
+      <addorganization />
   </span>
 </template>
 
