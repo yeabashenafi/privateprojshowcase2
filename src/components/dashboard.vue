b71d23d50f1a41dbb2d24477df4e6c60996084e4
minilik <minilikshumete@gmail.com>
2020-02-16 22:04:17 +0300

    new componsent crated and ....

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,17 +1,4 @@
 <template>
-  <v-layout row>
-    <v-card width="256" height="100%">
-      <v-navigation-drawer permanent height="100%">
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="title">
-              Profile
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              subtext
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
 
         <v-divider></v-divider>
 
